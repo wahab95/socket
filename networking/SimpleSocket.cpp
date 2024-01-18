9432e6a729a24802b396ba135ffc54cb16abf6bf
wahab95 <a.wahab1996@gmail.com>
2024-01-18 15:17:12 +0300

    Update

--- networking/SimpleSocket.cpp
+++ networking/SimpleSocket.cpp
@@ -1,8 +1,14 @@
 #include "SimpleSocket.hpp"
+#include <netinet/in.h>
 #include <sys/socket.h>
 
 
 
-hde::SimpleSocket::SimpleSocket(int domain, int service, int protocol){
+hde::SimpleSocket::SimpleSocket(int domain, int service, int protocol, int port, u_long interface){
+    // Define address structure:
+    address.sin_family = domain;
+    address.sin_port = htons(port);
+    address.sin_addr.s_addr=htons(interface);
+    // stablish connection:
     connection = socket(domain, service, protocol);
 }
--- networking/SimpleSocket.hpp
+++ networking/SimpleSocket.hpp
@@ -1,12 +1,17 @@
 #include <iostream>
 #include <sys/socket.h>
+#include <netinet/in.h>
+#include <sys/types.h>
 
 namespace hde {
     class SimpleSocket{
         private:
             int connection;
+            struct sockaddr_in address;
         public:
-            SimpleSocket(int domain, int service, int protocol);
+            SimpleSocket(int domain, int service, 
+            int protocol, int port, u_long interface);
+            virtual void establish_netwrok_connection()=0;
 
 
     };
